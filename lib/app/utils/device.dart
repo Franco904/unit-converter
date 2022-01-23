2dc5a28a8941c8b9dfc6d39054b94ddaba533cca
francostavares <francostavares2003@gmail.com>
2022-01-23 16:20:53 -0300

    Set preferred device orientations

--- lib/app/utils/device.dart
+++ lib/app/utils/device.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+
+Future<void> loadDeviceOrientation() async {
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+}
 
 bool isDeviceSmallerThan360dp(BuildContext context) {
   return MediaQuery.of(context).size.width < 360;
